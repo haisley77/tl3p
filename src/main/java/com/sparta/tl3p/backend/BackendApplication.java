364a7bc8ed9c5de7befc2e24e4d06c9f8ddb49b2
haisley77 <hahi1108@gmail.com>
2025-02-21 21:14:14 +0900

    mod: for jenkins ci success test

--- src/main/java/com/sparta/tl3p/backend/BackendApplication.java
+++ src/main/java/com/sparta/tl3p/backend/BackendApplication.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
-
-	jenkins
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
